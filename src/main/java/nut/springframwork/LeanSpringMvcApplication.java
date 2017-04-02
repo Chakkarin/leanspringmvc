1eb75af8fde69a156e175d4ea096cfbd45727ead
Chakkarin <chakkarin_yinghan@hotmail.com>
2017-04-02 10:08:07 +0700

    LearnSpringMVC

---
+++ src/main/java/nut/springframwork/LeanSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package nut.springframwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanSpringMvcApplication.class, args);
+	}
+}
